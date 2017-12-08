d92e2e057f9342e948440846ba64801c8e57146f
nishidat <nishidat@isao.co.jp>
2017-12-08 15:08:05 +0900

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!isset($_REQUEST["chl"])) {
 // QRコードを生成
 $qr = new Image_QRCode();
 $option = array(
-	"module_size"=>10,     //サイズ=>1〜19で指定
+	"module_size"=>5,     //サイズ=>1〜19で指定
 	"image_type"=>"png",   //画像形式=>jpegかpngを指定
 	"output_type"=>"display",  //出力方法=>displayかreturnで指定 returnの場合makeCodeで画像リソースが返される
 	"error_correct"=>"H"     //クオリティ(L<M<Q<H)を指定
